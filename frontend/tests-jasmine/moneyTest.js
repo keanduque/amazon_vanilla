27d53ff9245a9c5b6c35cd893e9174c456202643
Kean Duque <vizen_27@yahoo.com>
2025-11-19 12:29:30 +0000

    Remove default tests & add jasmine SpecRunner test

---
+++ frontend/tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { formatPrice } from "../scripts/utils/helpers.js";
+
+describe("Test suite: formatPrice", () => {
+  it("Converts Cents into Dollars", () => {
+    expect(formatPrice(2095)).toEqual("20.95");
+  });
+
+  it("Works with 0", () => {
+    expect(formatPrice(0)).toEqual("0.00");
+  });
+
+  it("Rounds up to the nearest cent", () => {
+    expect(formatPrice(2000.5)).toEqual("20.01");
+  });
+});
