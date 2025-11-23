01fa1a200a35fb5463bcd8b20fafa83281eb01f7
Kean Duque <vizen_27@yahoo.com>
2025-11-23 20:35:15 +0000

    make localStorageKey and load From private storage

--- frontend/data/cart-class.js
+++ frontend/data/cart-class.js
@@ -1,16 +1,16 @@
 import { validDeliveryOption } from "./deliveryOptions.js";
 
 class Cart {
-  cartItems;
-  localStorageKey;
+  cartItems; // public property
+  #localStorageKey; // private property
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
     if (!this.cartItems) {
       this.cartItems = [
         {
@@ -29,7 +29,7 @@ class Cart {
     }
   }
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
   addToCart({ productId, productName = "" }) {
     const qtySelectorEl = document.querySelector(
@@ -114,6 +114,8 @@ cart.addToCart({
   productName: "Luxury Towel Set - Graphite Gray",
 });
 
+// cart.#localStorageKey = "kean"; //Uncaught SyntaxError: Private field '#localStorageKey' must be declared in an enclosing class
+// cart.#localStorage(); // Uncaught SyntaxError: Private field '#localStorage' must be declared in an enclosing class
 console.log("cart", cart);
 
 const businessCart = new Cart("business-cart-class");
